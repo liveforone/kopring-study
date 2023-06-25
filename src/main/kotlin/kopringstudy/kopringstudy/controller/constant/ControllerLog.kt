27a4c9c4bf40f2b7ca1b85a8e3b24b3e714e2135
liveforone <yc4852@gmail.com>
2023-06-25 19:34:40 +0900

    Add controller log.

---
+++ src/main/kotlin/kopringstudy/kopringstudy/controller/constant/ControllerLog.kt
@@ -0,0 +1,7 @@
+package kopringstudy.kopringstudy.controller.constant
+
+enum class ControllerLog(val log: String) {
+    SIGNUP_SUCCESS("회원가입 성공"),
+    UPDATE_PASSWORD_SUCCESS("비밀번호 성공"),
+    ADMIN_ACCESS("!! Admin Access in Admin Page : ")
+}
