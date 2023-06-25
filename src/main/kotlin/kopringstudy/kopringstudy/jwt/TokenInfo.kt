8b39728aa544d055bd969cb8f0f8f850c22471cc
liveforone <yc4852@gmail.com>
2023-06-25 19:59:19 +0900

    Refactor constuctor shape.

--- src/main/kotlin/kopringstudy/kopringstudy/jwt/TokenInfo.kt
+++ src/main/kotlin/kopringstudy/kopringstudy/jwt/TokenInfo.kt
@@ -1,6 +1,10 @@
 package kopringstudy.kopringstudy.jwt
 
-class TokenInfo private constructor(val grantType: String, val accessToken:String, val refreshToken:String) {
+class TokenInfo private constructor(
+    val grantType: String,
+    val accessToken:String,
+    val refreshToken:String
+) {
     companion object {
         fun create(grantType:String, accessToken:String, refreshToken:String): TokenInfo {
             return TokenInfo(grantType, accessToken, refreshToken)
