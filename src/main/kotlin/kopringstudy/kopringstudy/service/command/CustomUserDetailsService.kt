3d8501bb5dbdafae918d00772773ede18437ebc1
liveforone <yc4852@gmail.com>
2023-06-25 20:07:18 +0900

    Add constant of role.

--- src/main/kotlin/kopringstudy/kopringstudy/service/command/CustomUserDetailsService.kt
+++ src/main/kotlin/kopringstudy/kopringstudy/service/command/CustomUserDetailsService.kt
@@ -10,24 +10,28 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class CustomUserDetailsService @Autowired constructor(private val memberRepository: MemberRepository) : UserDetailsService{
+class CustomUserDetailsService @Autowired constructor(
+    private val memberRepository: MemberRepository
+) : UserDetailsService{
 
     override fun loadUserByUsername(email: String): UserDetails {
         return createUserDetails(memberRepository.findOneByEmail(email))
     }
 
     private fun createUserDetails(member: Member): UserDetails {
+        val adminRole = "ADMIN"
+        val memberRole = "MEMBER"
         return if (member.auth == Role.ADMIN) {
             User.builder()
                 .username(member.identity)
                 .password(member.password)
-                .roles("ADMIN")
+                .roles(adminRole)
                 .build()
         } else {
             User.builder()
                 .username(member.identity)
                 .password(member.password)
-                .roles("MEMBER")
+                .roles(memberRole)
                 .build()
         }
     }
