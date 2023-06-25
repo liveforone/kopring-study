d24014fd349996d7da9ccba6a39c0cbe72ef1a79
liveforone <yc4852@gmail.com>
2023-06-25 19:19:08 +0900

    Add member url constant.

---
+++ src/main/kotlin/kopringstudy/kopringstudy/controller/constant/MemberUrl.kt
@@ -0,0 +1,11 @@
+package kopringstudy.kopringstudy.controller.constant
+
+object MemberUrl {
+    const val SIGNUP = "/user/signup"
+    const val LOGIN = "/user/login"
+    const val MEMBER_INFO = "/user/info"
+    const val SEARCH_MEMBER = "/user/search"
+    const val UPDATE_PASSWORD = "/user/update-password"
+    const val ALL_MEMBER_ADMIN_PAGE = "/user/find-all"
+    const val PROHIBITION = "/prohibition"
+}
