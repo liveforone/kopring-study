79e77189e52e5e8b6b4dfbdcbe3fd94e62f9406f
liveforone <yc4852@gmail.com>
2023-06-25 20:00:53 +0900

    Refactor shape of kotlin convention.

--- src/main/kotlin/kopringstudy/kopringstudy/repository/MemberCustomRepository.kt
+++ src/main/kotlin/kopringstudy/kopringstudy/repository/MemberCustomRepository.kt
@@ -4,8 +4,8 @@ import kopringstudy.kopringstudy.domain.Member
 import kopringstudy.kopringstudy.dto.MemberResponse
 
 interface MemberCustomRepository {
-    fun findOneByEmail(email: String) :Member
-    fun findOneByIdentity(identity:String):Member
+    fun findOneByEmail(email: String): Member
+    fun findOneByIdentity(identity: String): Member
     fun findAllMember(): List<MemberResponse>
-    fun searchMember(keyword:String): List<MemberResponse>
+    fun searchMember(keyword: String): List<MemberResponse>
 }
