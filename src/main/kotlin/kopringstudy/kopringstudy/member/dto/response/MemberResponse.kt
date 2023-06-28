e391b82038aab87128fb74edd1e60982117dc28e
liveforone <yc4852@gmail.com>
2023-06-28 14:25:13 +0900

    Add entity to dto method in response dto.

--- src/main/kotlin/kopringstudy/kopringstudy/member/dto/response/MemberResponse.kt
+++ src/main/kotlin/kopringstudy/kopringstudy/member/dto/response/MemberResponse.kt
@@ -1,5 +1,6 @@
 package kopringstudy.kopringstudy.member.dto.response
 
+import kopringstudy.kopringstudy.member.domain.Member
 import kopringstudy.kopringstudy.member.domain.Role
 
 data class MemberResponse(
@@ -8,4 +9,16 @@ data class MemberResponse(
     var name: String,
     var age: Int,
     var auth: Role
-)
+) {
+    companion object {
+        fun entityToDto(member: Member): MemberResponse {
+            return MemberResponse(
+                member.id,
+                member.identity,
+                member.name,
+                member.age,
+                member.auth
+            )
+        }
+    }
+}
--- src/main/kotlin/kopringstudy/kopringstudy/member/service/query/MemberQueryService.kt
+++ src/main/kotlin/kopringstudy/kopringstudy/member/service/query/MemberQueryService.kt
@@ -1,7 +1,6 @@
 package kopringstudy.kopringstudy.member.service.query
 
 import kopringstudy.kopringstudy.member.dto.response.MemberResponse
-import kopringstudy.kopringstudy.member.dto.util.MemberMapper
 import kopringstudy.kopringstudy.member.repository.MemberRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -14,11 +13,11 @@ class MemberQueryService @Autowired constructor(
 ) {
 
     fun getMemberByIdentity(identity:String): MemberResponse {
-        return MemberMapper.entityToDto(memberRepository.findOneByIdentity(identity))
+        return MemberResponse.entityToDto(memberRepository.findOneByIdentity(identity))
     }
 
     fun getMemberByEmail(email: String): MemberResponse {
-        return MemberMapper.entityToDto(memberRepository.findOneByEmail(email))
+        return MemberResponse.entityToDto(memberRepository.findOneByEmail(email))
     }
 
     fun searchMember(keyword:String): List<MemberResponse> = memberRepository.searchMember(keyword)
